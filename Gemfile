1cae20b7e4490d62c1194b1387798669122c2252
Justyna Klos <klos.justyna@wp.pl>
2018-11-03 13:52:55 +0100

    loofah

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.3.3'
  gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+ gem 'loofah'
  #Add devise
  gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_035059) do
+ActiveRecord::Schema.define(version: 2018_11_03_041455) do
 
   create_table "tickets", force: :cascade do |t|
     t.datetime "start_date"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2018_11_03_035059) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
-
-ActiveRecord::Schema.define(version: 2018_11_03_041455) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
