29077a09aa1367f3f5e82dd400d381519c9e4ebf
Damian Domzalski <damiandomzalskipl@gmail.com>
2018-12-10 21:38:28 +0100

    schema version update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_190225) do
+ActiveRecord::Schema.define(version: 2018_12_10_191123) do
 
   create_table "abilities", force: :cascade do |t|
     t.datetime "start_time"
